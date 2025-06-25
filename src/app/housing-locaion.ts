e1c7a0e199f458fb3b4cf46161598998c8691d0d
Brian <Brian.Edward.Hackett@gmail.com>
2025-06-25 04:13:34 +0000

    Add Home and Housing Location components
    - Modified: app.component.ts
    - Added: home.component.css, home.component.spec.ts, home.component.ts
    - Added: housing-locaion.ts
    - Added: housing-location.component.css, housing-location.component.spec.ts, housing-location.component.ts

---
+++ src/app/housing-locaion.ts
@@ -0,0 +1,10 @@
+export interface HousingLocaion {
+    id:number,
+    name:string,
+    city:string,
+    state:string,
+    photo:string,
+    availableUnits:number,
+    hasWifi:boolean,
+    hasLaundry:boolean
+}
